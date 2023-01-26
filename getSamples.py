f5e81ce2bcb0a6b44b43a24631ad619448fb956c
Ahmad Jawabreh <98946028+Jawabreh0@users.noreply.github.com>
2023-01-26 15:15:52 +0200

    Update getSamples.py

--- getSamples.py
+++ getSamples.py
@@ -4,7 +4,7 @@ from simple_image_download import simple_image_download as simp
 response = simp.simple_image_download
 
 # the keywords that will be used to find pics, and each key work will create a different file 
-keywords = ["AR-15","AR15"]
+keywords = ["Motor Helmet","Bike Helmet"]
 
 # for loop on the keywords
 # (kw, 300) means 300 sample of each keyword
